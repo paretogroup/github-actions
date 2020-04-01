09ed99ae63cdef354dce2e1edd77d529c772a6b5
felipereyel <felipe.reyel@gmal.com>
2020-04-01 15:27:07 -0300

    action: add label to issue

---
+++ add-label-to-issue/README.md
@@ -0,0 +1,25 @@
+# Add label to issue
+
+Adds a specific label to a given issue
+
+## Inputs
+
+### `token`
+
+**Required** The token.
+
+### `issue-id`
+
+**Required** The issue id.
+
+### `organization-name`
+
+**Required** The organization name.
+
+### `repository-name`
+
+**Required** The repository name.
+
+### `label`
+
+**Required** The label to be added.
---
+++ add-label-to-issue/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    try {
+        const token = core.getInput('token', { required: true });
+        const issueId = core.getInput('issue-id',  { required: true });
+        const organizationName = core.getInput('organization-name',  { required: true });
+        const repositoryName = core.getInput('repository-name',  { required: true });
+        const label = core.getInput('label',  { required: true });
+
+        const client = new github.GitHub(token);
+        const card = await client.projects.getCard({ card_id: cardId.toString() });
+
+        await client.issues.addLabels({
+            owner: organizationName,
+            repo: repositoryName,
+            issue_number: issueId,
+            labels: [label]
+        });
+
+        core.info(`Added label "${label}" to issue #${issueId} in repository ${organizationName}/${repositoryName}.`);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
